db1836d3d9e311f17f998a9246894ee7b51aa710
Abrar Saad <abrarsaad3572022@gmail.com>
2023-12-06 15:37:33 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @ac: integer variable that stores the number of command-line arguments.
+ * @argv: is an array of character pointers listing all the arguments.
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char **argv)
+{
+return (0);
+}
