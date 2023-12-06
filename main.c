f0a233e00c9875a34993d0d07730dc5976718ede
Abrar Saad <abrarsaad3572022@gmail.com>
2023-12-06 16:39:56 +0200

    add .gitignore file - modify style of main.c file

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @ac: integer variable that stores the number of command-line arguments.
- * @argv: is an array of character pointers listing all the arguments.
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* @ac: integer variable that stores the number of command-line arguments.
+* @argv: is an array of character pointers listing all the arguments.
+* Return: Always 0 (Success)
+*/
 int main(int ac, char **argv)
 {
-char *prompt = "(Ashell)$";
-printf("%s", prompt);
-return (0);
+(void)ac;
+(void)argv;
+	char *prompt = "(Ashell)$";
+
+	printf("%s", prompt);
+	return (0);
 }
