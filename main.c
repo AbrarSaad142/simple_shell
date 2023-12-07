455b13bc4e2d7d3de00255eb4bff1ef934df34dd
boodenator <boode1000@gmail.com>
2023-12-07 07:44:33 -0800

    main.c

--- main.c
+++ main.c
@@ -9,6 +9,20 @@
 int main(int ac, char **argv)
 {
 char *prompt = "(Ashell)$";
+char *linetr;
+size_t n = 0;
+ssize_t our_var; /*variable for getline function*/
+(void)ac; (void)argv; /*void variables*/
+
+while(1){
 printf("%s", prompt);
+our_var = getline(&lineptr, &n, &stdin);/*check if getline function failed*/
+if(our_var == -1) {
+	printf("ERROR!...");/*error message if function failed*/
+	return(-1);
+}
+printf("%s\n", lineptr);
+free(lineptr);/*free-up allocated memory*/
+}
 return (0);
 }
