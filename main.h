4f70f5236422071279dd15e63909ec7a8e680d73
boodenator <boode1000@gmail.com>
2023-12-09 04:46:44 -0800

    updated

--- main.h
+++ main.h
@@ -2,11 +2,12 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 void execmd(char **argv);
 char *get_location(char *command);
