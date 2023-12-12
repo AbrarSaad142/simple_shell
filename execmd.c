a94b3226a934797d561dc7e16b3ada4075494e02
Abrar Saad <abrarsaad3572022@gmail.com>
2023-12-12 03:28:05 +0200

    task 3 Handle path

--- execmd.c
+++ execmd.c
@@ -3,9 +3,19 @@
 /**
  * execmd- this function to execude the command.
  * @argv: array of string.
-*/
+ */
 void execmd(char **argv)
 {
+pid_t pid = fork();
+
+if (pid < 0)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/*Child process*/
 char *command = NULL, *actual_command = NULL;
 
 if (argv)
@@ -13,11 +23,24 @@ if (argv)
 command = argv[0];
 actual_command = get_location(command);
 
+if (actual_command == NULL)
+{
+fprintf(stderr, "Command not found: %s\n", command);
+exit(EXIT_FAILURE);
+}
+
 if (execve(actual_command, argv, NULL) == -1)
 {
-perror("Error: ");
+perror("execve");
+exit(EXIT_FAILURE);
 }
-
 }
-
+}
+else
+{
+/*Parent process*/
+int status;
+waitpid(pid, &status, 0);
+/* You can add additional logic for handling the exit status if needed.*/
+}
 }
